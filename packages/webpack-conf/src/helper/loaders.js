34dbfcea0892d1099e68ba3e8582294d5a57f41d
Sergey Bondar <sergeyb@xcritical.software>
2021-11-02 12:05:36 +0300

    feat(webpack-conf): Update html-webpack-plugin mini-css-extract-plugin versions

--- packages/webpack-conf/src/helper/loaders.js
+++ packages/webpack-conf/src/helper/loaders.js
@@ -1,6 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const xcFrontBabel = require('../../../babelify/index');
+const xcFrontBabel = require('@xcritical/babelify');
 
 const paths = require('./paths');
 const env = require('./env');
